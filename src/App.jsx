682974a4018e39bab958a59b596fb19ff89f322a
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2023-11-20 22:28:49 -0600

    Vídeo 54 Creando y llamando nuestro primer Componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const edad = 18;
-
-  const sumar = () => {
-    const numero = 5;
-    if (numero > 5) {
-      console.log("Si es mayor a 5");
-    } else {
-      console.log("No es mayor a 5");
-    }
-  };
-
-  sumar();
-
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        {1 + 1}
-        {edad >= 18 ? "Eres mayor de edad" : "No eres mayor de edad"}
-        <h2>{"Me llamo Daniel".toLocaleLowerCase()}</h2>
-        <h1>Hola mundo</h1>
-      </div>
+      <Header/>
     </>
   );
 }
