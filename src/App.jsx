712605f1a863acfffd6832043f1be3e0fb1a556c
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2023-11-20 22:06:29 -0600

    Vídeo 53 JavaScript en HTML con JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const edad = 18;
+
+  const sumar = () => {
+    const numero = 5;
+    if (numero > 5) {
+      console.log("Si es mayor a 5");
+    } else {
+      console.log("No es mayor a 5");
+    }
+  };
+
+  sumar();
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        {1 + 1}
+        {edad >= 18 ? "Eres mayor de edad" : "No eres mayor de edad"}
+        <h2>{"Me llamo Daniel".toLocaleLowerCase()}</h2>
         <h1>Hola mundo</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
