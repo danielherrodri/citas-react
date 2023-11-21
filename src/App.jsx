bb7c082a975bc8f7e551404862054803859e3aee
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2023-11-20 22:46:58 -0600

    Vídeo 56 React Developer Tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
     </>
   );
 }
